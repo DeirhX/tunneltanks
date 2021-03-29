6d6b0d549197de5dd1eddd934916139094a6ee98
Ondřej Dobiáš <ondrej.dobias@avast.com>
2021-03-29 02:03:31 +0200

    Compilable with nanogui-sdl2

--- src/screen.cpp
+++ src/screen.cpp
@@ -6,9 +6,82 @@
 #include "world.h"
 #include "game_system.h"
 
+#pragma warning(push)
+#pragma warning(disable: 5054)
+#include <nanogui/nanogui.h>
+#pragma warning(pop)
+
 /* The constructor sets the video mode: */
 Screen::Screen(bool is_fullscreen, ScreenRenderSurface * render_surface) : is_fullscreen(is_fullscreen), screen_surface(render_surface)
 {
+
+    enum test_enum
+    {
+        Item1 = 0,
+        Item2,
+        Item3
+    };
+
+    bool bvar = true;
+    int ivar = 12345678;
+    double dvar = 3.1415926;
+    float fvar = (float)dvar;
+    std::string strval = "A string";
+    test_enum enumval = Item2;
+    nanogui::Color colval(0.5f, 0.5f, 0.7f, 1.f);
+
+
+    nanogui::init();
+
+    /* scoped variables */ {
+        bool use_gl_4_1 = false; // Set to true to create an OpenGL 4.1 context.
+        nanogui::Screen * screen = nullptr;
+
+        if (use_gl_4_1)
+        {
+            // NanoGUI presents many options for you to utilize at your discretion.
+            // See include/nanogui/screen.h for what all of these represent.
+            screen = new nanogui::Screen(nanogui::Vector2i(500, 700), "NanoGUI test [GL 4.1]",
+                                /*resizable*/ true, /*fullscreen*/ false, /*colorBits*/ 8,
+                                /*alphaBits*/ 8, /*depthBits*/ 24, /*stencilBits*/ 8,
+                                /*nSamples*/ 0, /*glMajor*/ 4, /*glMinor*/ 1);
+        }
+        else
+        {
+            screen = new nanogui::Screen(nanogui::Vector2i(500, 700), "NanoGUI test");
+        }
+
+        bool enabled = true;
+        nanogui::FormHelper * gui = new nanogui::FormHelper(screen);
+        nanogui::ref<nanogui::Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+        gui->addGroup("Basic types");
+        gui->addVariable("bool", bvar);
+        gui->addVariable("string", strval);
+
+        gui->addGroup("Validating fields");
+        gui->addVariable("int", ivar)->setSpinnable(true);
+        gui->addVariable("float", fvar);
+        gui->addVariable("double", dvar)->setSpinnable(true);
+
+        gui->addGroup("Complex types");
+        gui->addVariable("Enumeration", enumval, enabled)->setItems({"Item 1", "Item 2", "Item 3"});
+        gui->addVariable("Color", colval)->setFinalCallback([](const nanogui::Color & c) {
+            std::cout << "ColorPicker Final Callback: [" << c.r() << ", " << c.g() << ", " << c.b() << ", " << c.w()
+                      << "]" << std::endl;
+        });
+
+        gui->addGroup("Other widgets");
+        gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; });
+
+        screen->setVisible(true);
+        screen->performLayout();
+        window->center();
+
+        nanogui::mainloop();
+    }
+
+    nanogui::shutdown();
+
     this->Resize(screen_surface->GetSize());
 }
 
