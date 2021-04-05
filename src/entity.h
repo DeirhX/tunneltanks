612fa26d9268fd46876a3587f63bc090aff52351
Ondřej Dobiáš <ondrej.dobias@avast.com>
2021-04-05 16:05:59 +0200

    Begin to lay down entity framework

---
+++ src/entity.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "containers.h"
+
+class Entity
+{
+public:
+    
+};
+
+template <typename TAdvanceable>
+concept Advanceable = requires(TAdvanceable t)
+{
+    t.Advance();
+};
+
+class Component
+{
+    
+};
+
+/*
+template <typename... TComponent>
+class ComponentList
+{
+    MultiTypeContainer<TComponent> list;
+};
+*/
